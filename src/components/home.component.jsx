9711686ef056e9ca356eb506a42a18dbbf7f0dcf
Erick Trevino <erick.trevino@cloudlite.io>
2023-11-23 14:57:41 -0600

    Update home and navigation component & styling.

--- src/components/home.component.jsx
+++ src/components/home.component.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
 import NavigationMenu from './navigation.component';
-import Carousel from './carousel.component';
+import Footer from './footer.component';
+import './home.styles.css';
 
 function Home() {
   return (
@@ -9,9 +11,14 @@ function Home() {
       <div id="main-header-bg">
         <div>
           <h1 id="main-big-header">Clean</h1>
-          <h2 id="main-subtitle">Residential - Commercial - Everything</h2>
+          <h2 id="main-subtitle">Precision in Every Detail, Perfection in Every Cleanse</h2>
         </div>
       </div>
+      <div id="welcome">
+        <h3>Welcome to STX Cleaning - Your Trusted Cleaning Partner</h3>
+        <p>At STX Cleaning, we understand the importance of a clean and healthy living or working environment. With our professional cleaning services, we take the hassle out of maintaining a pristine space, allowing you to focus on what matters most.</p>
+      </div>
+      <Footer dark facebookLink="https://www.facebook.com" twitterLink="https://www.twitter.com" instagramLink="https://www.instagram.com" />
     </>
   );
 }
--- src/components/navigation.component.jsx
+++ src/components/navigation.component.jsx
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { Link, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBroom } from '@fortawesome/free-solid-svg-icons';
 
 function NavigationMenu() {
   const location = useLocation();
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [transparent, setTransparent] = useState(true);
+
+  const handleScroll = () => {
+    setScrollPosition(window.scrollY);
+  };
+
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (scrollPosition > 0) {
+      setTransparent(false);
+    } else {
+      setTransparent(true);
+    }
+  }, [scrollPosition]);
 
   return (
-    <nav id="main-navigation" className="navbar fixed-top navbar-expand-lg" data-bs-theme="dark">
+    <nav id={transparent ? 'main-navigation-transparent' : 'main-navigation'} className="navbar fixed-top navbar-expand-lg">
       <div className="container-fluid w-75">
         <Link to="/" className="navbar-brand">
-          <img src="/assets/images/clean-color.png" id="main-header-logo" alt="logo" />
-          <span id="header-brand">STX Cleaners</span>
+          <FontAwesomeIcon icon={faBroom} id={transparent ? 'main-header-logo-transparent' : 'main-header-logo'} />
+          {/* <img src="/assets/images/clean-color.png" id="main-header-logo" alt="logo" /> */}
+          <span id={transparent ? 'header-brand-transparent' : 'header-brand'}>STX Cleaning</span>
         </Link>
         <div className="d-flex">
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className={location.pathname === '/' || location.pathname === '' ? 'nav-link active' : 'nav-link'} to="/">Home</Link>
+                <Link
+                  className={`nav-link ${location.pathname === '/' || location.pathname === '' ? 'active' : ''} ${transparent ? 'transparent-link' : ''}`}
+                  to="/"
+                >
+                  Home
+                </Link>
               </li>
               {/* <li className="nav-item">
                 <Link className={location.pathname === 'about' ? 'nav-link active' : 'nav-link'} to="/about">About</Link>
